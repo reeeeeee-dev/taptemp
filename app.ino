ddece8fd827c43cf8a75a2ac93ad65b12054cf98
Reetik Patel <enceledus@Esfera-Triumph.local>
2020-10-14 17:57:28 -0500

    🚧➕ Setup DHT module

--- app.ino
+++ app.ino
@@ -1,9 +1,19 @@
 #include <LiquidCrystal.h>
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
+    dht.begin();
   lcd.begin(16, 2);
   lcd.print("hello, world!");
 }
 
 void loop() {
+    float hum = dht.readHumidity();
+    float temp = dht.readTemperature(true);
 }
